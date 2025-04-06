66159570ecfb7075c545f9358ca4f87eeca539a4
Priyanshu7798 <124646393+Priyanshu7798@users.noreply.github.com>
2025-04-06 17:52:31 +0530

    implemented interview UI page

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -14,7 +14,7 @@ export default async function RootLayout({children}:{children:ReactNode}){
         <>
             <div className="root-layout">
                 <nav>
-                    <Link href="/" className="flex items-center gap-2 ">
+                    <Link href="/" className="flex items-center gap-2 mb-5 ">
                         <Image 
                             src='logo.svg' alt="logo" width={38} height={32}
                         />
